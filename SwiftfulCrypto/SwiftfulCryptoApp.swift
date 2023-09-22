9ce30563e5b2884d29724d54891fc5bf13d8fff0
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-09-22 22:46:16 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Leonardo Almeida on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
